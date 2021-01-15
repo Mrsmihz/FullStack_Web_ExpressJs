7c86b6e569c285cfc9cc16264a6a4adf406afba3
Faith <Dechapon.n@hotmail.com>
2021-01-16 04:20:05 +0700

    code refactor

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,28 +1,8 @@
 const express = require('express');
 const router = express.Router();
-var sql = require('./my_db');
 
 router.get('/', (req, res) => {
-    var stmt = 'select * from person';
-    sql.query(stmt, function(err, result){
-        if (err){
-            console.log(err);
-        }
-        else{
-            res.send(result);
-        }
-    })
-});
-router.post('/', function(req, res){
-    var stmt = 'insert into person (first_name, last_name) values(?,?)';
-    sql.query(stmt, [req.body.first_name, req.body.last_name], function(err, res){
-        if (err){
-            console.log(err);
-        }
-        else{
-            console.log(res);
-        }
-    })
+   res.send('test get')
 })
 
 
