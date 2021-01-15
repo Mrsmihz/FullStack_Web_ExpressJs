cbe5af3a4de9e74ee8c5df1b7eb54bb70422bacf
Faith <Dechapon.n@hotmail.com>
2021-01-16 04:20:28 +0700

    change to use account controller instead

--- server/routes/api/register.js
+++ server/routes/api/register.js
@@ -1,21 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const sql = require('./my_db');
+const account = require('../../Controller/Account');
 
 router.get('/', function(req, res){
-
+    res.sendStatus(404);
 })
-router.post('/', function(req, res){
-    var stmt = 'insert into Accounts(first_name, last_name, username, password, email) values(?,?,?,?,?)';
-    var data = JSON.parse(req.body.json);
-    sql.query(stmt, [data.first_name, data.last_name, data.username, data.password, data.email], function(err, result){
-        if (err){
-            res.send('failed');
-        }
-        else{
-            res.send('success');
-        }
-    })
-})
+router.post('/', account.createAccount);
 
 module.exports = router;
