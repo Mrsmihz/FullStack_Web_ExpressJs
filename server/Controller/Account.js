01a865603cbdbba03f56e33506cf32586a7bb6b3
Faith <Dechapon.n@hotmail.com>
2021-01-16 04:19:09 +0700

    add Account controller

---
+++ server/Controller/Account.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const sql = require('./my_db');
+exports.createAccount = function(req, res){
+    var stmt = 'insert into Accounts(first_name, last_name, username, password, email) values(?,?,?,?,?)';
+    var data = JSON.parse(req.body.json);
+    sql.query(stmt, [data.first_name, data.last_name, data.username, data.password, data.email], function(err, result){
+        if (err){
+            res.send('FAIL');
+        }
+        else{
+            res.send('OK');
+        }
+    })
+}
+
+exports.accountLogin = function(req, res){
+    var stmt = "select id, username, password, first_name, last_name, email from Accounts where username= ? ";
+    var data = JSON.parse(req.body.json);
+    sql.query(stmt, [data.username], function(err, result){
+        if (err){
+            res.send('FAIL');
+        }
+        else{
+            var query = result[0];
+            if (query.password === data.password){
+                var newUser = {
+                    id:query.id,
+                    username:query.username,
+                    password:query.password,
+                    first_name:query.first_name,
+                    last_name:query.last_name,
+                    email:query.email
+                };
+                req.session.user = newUser;
+                console.log(req.session.user);
+                res.send('OK');
+            }
+            else{
+                res.send('FAIL');
+            }
+        }
+    })
+}
+
+exports.getSession = function(req, res){
+    var store = req.sessionStore;
+    if (store.sessions){
+        for (var sid in store.sessions){
+            var ses = JSON.parse(store.sessions[sid]);
+            console.log(ses.user);
+        }
+    }
+}
