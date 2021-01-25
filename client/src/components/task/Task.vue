bd67e490dee69d4b8b9334f725e11ad4c0dd84d5
Mrsmithz <dechapon.n@hotmail.com>
2021-01-26 04:28:29 +0700

    add task template but didn't use for now

---
+++ client/src/components/task/Task.vue
@@ -0,0 +1,34 @@
+<template v-for="task in this.tasks">
+    <div>
+        <b-card
+        :title="task.title">
+        </b-card>
+        <b-card-text>
+            {{ task.desc }}
+            <br/>
+            {{ task.owner }}
+        </b-card-text>
+    </div>
+</template>
+<script>
+export default {
+    name:'Task',
+    data(){
+        return {
+            tasks:{
+                a:{
+                    title:'tesdf',
+                    desc:'askd',
+                    owner:'1'
+                }
+            }
+        }
+    },
+    components:{
+
+    },
+    methods:{
+
+    }
+}
+</script>
