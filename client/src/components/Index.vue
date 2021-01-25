7583073d87d103d9003b5fe98637863b6306c1bc
Mrsmithz <dechapon.n@hotmail.com>
2021-01-26 04:27:48 +0700

    add templates and method

--- client/src/components/Index.vue
+++ client/src/components/Index.vue
@@ -1,18 +1,16 @@
 <template>
     <div>
-        <h1>{{username}}</h1>
-        <h1>{{password}}</h1>
-        <h1>{{first_name}}</h1>
-        <h1>{{last_name}}</h1>
-        <h1>{{email}}</h1>
-        <b-form method="get" @submit="logout">
-            <b-button type="submit" class="mt-3">Logout</b-button>
-        </b-form>
+        <Navbar ref="navbar"/>
+        <createTask v-show="createTask" ref="createtask"/>
+        <showTask v-if="showTask" ref="showTask" class="mt-3"/>
     </div>
 </template>
 
 <script>
 import AccountService from '../services/AccountService';
+import Navbar from './Navbar'
+import createTask from '../components/task/createTask'
+import showTask from '../components/task/showTask'
 
 export default {
     name:'Index',
@@ -22,19 +20,42 @@ export default {
             password:'',
             first_name:'',
             last_name:'',
-            email:''
+            email:'',
+            id:'',
+            createTask:false,
+            showTask:false,
         }
+    },
+    components:{
+        Navbar,
+        createTask,
+        showTask
     },
     methods:{
         setUser:function(data){
+            this.id = data.id;
             this.username = data.username;
             this.password = data.password;
             this.first_name = data.first_name;
             this.last_name = data.last_name;
             this.email = data.email;
+            this.$refs.navbar.setUser(data);
+            this.$nextTick(function(){
+                this.showTask = true;
+            })
         },
         logout:async function(){
             await AccountService.logout();
+        },
+        showCreateTask:function(){
+            if (this.createTask){
+                this.createTask = false;
+                this.showTask = true;
+            }
+            else{
+                this.showTask = false;
+                this.createTask = true;
+            }
         }
     }
 
